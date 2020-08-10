b5f5230acc2d1cdb2b8f5b14890d734762ab3517
Chris Aga <chrisaga.dev@gmail.com>
2020-08-09 23:49:30 -0500

    Further setup on UserProfileInfo

--- basic_app/models.py
+++ basic_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class UserProfileInfo(models.Model):
@@ -7,3 +8,12 @@ class UserProfileInfo(models.Model):
   # password
   # firstname
   # surname
+
+  user = models.OneToOneField(User, on_delete=models.PROTECT)
+
+  portfolio_site = models.URLField(blank=True)
+
+  profile_pic = models.ImageField(upload_to="profile_pics", blank=True)
+
+  def __str__(self):
+    return self.user.username
